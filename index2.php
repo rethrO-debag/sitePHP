4f4b694255f4dabb72cf653a49777a096cbc0bc4
rethrO-debag <55383521+rethrO-debag@users.noreply.github.com>
2024-03-26 21:02:56 +0500

    Made some changes

--- index2.php
+++ index2.php
@@ -1,19 +1,14 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Document</title>
 </head>
-<header></header>
 
 <body>
-    <p>Test1</p>
-    <?php
-
-    ?>
-
+    <p>Test</p>
 </body>
 
 </html>
