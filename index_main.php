de1f19f45a21cbf7ac3f1c054aa8a8d47ccb1e2f
rethrO-debag <vadim.mahmutov747@mail.ru>
2024-03-29 16:51:52 +0500

    index.php connect

--- index_main.php
+++ index_main.php
@@ -8,7 +8,44 @@
 </head>
 
 <body>
+    <?php
+        $connection = pg_connect("host=localhost dbname=school user=postgres password=1234");
+        if (!$connection){
+            echo "An error occured.<br>";
+            exit;
+        }
+        $result = pg_query($connection, "Select * from students");
+        if (!$result){
+            echo "An error occured.<br>";
+            exit;
+        }
+    ?>
 
+<table>
+        <tr>
+            <td>student_id</td>
+            <td>name</td>
+            <td>first_name</td>
+            <td>second_name</td>
+            <td>date_of_birth</td>
+            <td>class_id</td>
+        </tr>
+
+        <?php
+            while($row = pg_fetch_assoc($result)){
+                echo "
+                <tr>
+                    <td>$row[student_id]</td>
+                    <td>$row[name]</td>
+                    <td>$row[first_name]</td>
+                    <td>$row[second_name]</td>
+                    <td>$row[date_of_birth]</td>
+                    <td>$row[class_id]</td>
+                </tr>
+                ";
+            }
+        ?>
+    </table>
 </body>
 
 </html>
